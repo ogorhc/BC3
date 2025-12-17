cfa0a810bfe1006974c028960687b44d94b1f09c
ogorhc <igorhinojosacastro@gmail.com>
2025-12-17 10:08:09 +0100

    feat: add TODO for implementing input type strategies in BC3 parser

--- src/api/bc3.ts
+++ src/api/bc3.ts
@@ -3,6 +3,7 @@ import { parseBC3 } from '../parsing/parseBC3.js';
 
 export class BC3 {
   static parse(input: string, options: ParseOptions = {}): ParseResult {
+    //TODO: Implement strategies for different input types
     return parseBC3({
       source: { type: 'string' },
       content: input,
